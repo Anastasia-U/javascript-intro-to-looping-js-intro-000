c488f1b844f2e4d2945ee2eea0dd85831314c9ba
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-07-13 01:01:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++){
     if (i === 1) {
-      return array.push(`I am ${i} strange loop`);
+      array.push(`I am ${i} strange loop`);
     }
     else {
-      return array.push(`I am ${i} strange loop.`);
+      array.push(`I am ${i} strange loop.`);
     }
   }
   return array;
